c7f1f5060dfb4ab9cef00c07ef305b2ae3938f9e
aspecky <60469830+Aspecky@users.noreply.github.com>
2024-09-17 03:12:21 +0100

    added arrow support

--- volume-plus/README.md
+++ volume-plus/README.md
@@ -3,12 +3,11 @@
 Overrides Spotify's default volume mouse wheel control and adds a tooltip to see and modify your volume.
 
 ### By default:
-* Mouse wheel increments volume by 5%
-* Holding <kbd>Shift</kbd> while using the mouse wheel will increment the volume by 10%
+* Mouse wheel and left/right arrows increment volume by 5%
+* Holding <kbd>Shift</kbd> while using the mouse wheel or up/down arrows increment the volume by 10%
 * Holding <kbd>Ctrl</kbd> while using the mouse wheel will increment the volume by 1%
 
-You may customize how much the scroll wheel increments the volume by clicking your profile picture top right and selecting `Volume+`
-
+You may customize the volume increments by clicking your profile picture top right and selecting `Volume+`
 
 ## Made with Spicetify Creator
 - https://github.com/spicetify/spicetify-creator
--- volume-plus/src/app.tsx
+++ volume-plus/src/app.tsx
@@ -139,17 +139,17 @@ async function main() {
 	new Menu.Item(
 		"Volume+",
 		false,
-			() => {
-				const container = document.createElement("div");
-				container.id = settings.settingsId;
-				ReactDOM.render(<settings.FieldsContainer />, container);
+		() => {
+			const container = document.createElement("div");
+			container.id = settings.settingsId;
+			ReactDOM.render(<settings.FieldsContainer />, container);
 
-				PopupModal.display({
-					title: "Volume+",
-					content: container,
-					isLarge: true,
-				});
-			},
+			PopupModal.display({
+				title: "Volume+",
+				content: container,
+				isLarge: true,
+			});
+		},
 		`<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="volume-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 16 16" style="enable-background:new 0 0 16 16;" xml:space="preserve"><path d="M14.9,8c0-0.4-0.3-0.8-0.7-0.8c0,0,0,0,0,0l-2.1,0V5.2c0-0.4-0.4-0.7-0.8-0.7c-0.4,0-0.7,0.3-0.7,0.7l0,2.1H8.5
     C8.1,7.2,7.7,7.6,7.7,8c0,0.4,0.3,0.8,0.7,0.8c0,0,0,0,0.1,0h2.1v2.1c0,0.4,0.4,0.7,0.8,0.7c0.4,0,0.7-0.3,0.7-0.7l0-2.1h2.1
     C14.6,8.8,14.9,8.4,14.9,8z"/><path d="M10.1,1.5c0-0.4-0.3-0.8-0.7-0.8c-0.1,0-0.3,0-0.4,0.1l-6.9,4c-1.7,1-2.3,3.2-1.3,5c0.3,0.6,0.8,1,1.3,1.3l6.9,4    c0.4,0.2,0.8,0.1,1-0.3c0.1-0.1,0.1-0.2,0.1-0.4v-1.9c-0.5-0.1-1-0.4-1.5-0.7v1.3L2.8,9.9C1.8,9.3,1.4,8,2,6.9    c0.2-0.3,0.5-0.6,0.8-0.8l5.8-3.3v1.3c0.4-0.3,1-0.5,1.5-0.7V1.5z"/></svg>`,
@@ -157,32 +157,14 @@ async function main() {
 
 	// MARK: Mouse wheel listener
 	let debounce = false;
-	container.addEventListener("wheel", (ev) => {
-		// override spotify's default mouse wheel listener
-		ev.stopPropagation();
 
-		// debounce so that the volume slider doesnt jump back and forth
+	function stepVolume(step: number) {
 		if (debounce) {
 			return;
 		}
 		debounce = true;
 
-		let field;
-		if (ev.getModifierState("Shift")) {
-			field = "shift-increment";
-		} else if (ev.getModifierState("Control")) {
-			ev.preventDefault(); // prevents zooming
-			field = "ctrl-increment";
-		} else {
-			field = "default-increment";
-		}
-
-		const step = Number(settings.getFieldValue(field)) / 100;
-		const newVolume = clamp(
-			PlaybackAPI._volume - Math.sign(ev.deltaY) * step,
-			0,
-			1,
-		);
+		const newVolume = clamp(PlaybackAPI._volume + step, 0, 1);
 		PlaybackAPI.setVolume(newVolume);
 
 		if (newVolume == 0 || newVolume == 1) {
@@ -196,6 +178,52 @@ async function main() {
 			},
 			{ once: true },
 		);
+	}
+
+	container.addEventListener("wheel", (ev) => {
+		ev.stopPropagation();
+
+		let field;
+		if (ev.getModifierState("Shift")) {
+			field = "shift-increment";
+		} else if (ev.getModifierState("Control")) {
+			ev.preventDefault(); // prevents zooming
+			field = "ctrl-increment";
+		} else {
+			field = "default-increment";
+		}
+
+		const step = Number(settings.getFieldValue(field)) / 100;
+		stepVolume(-Math.sign(ev.deltaY) * step);
+	});
+
+	document.addEventListener("keydown", (ev) => {
+		if (
+			(ev.key == "ArrowLeft" ||
+				ev.key == "ArrowRight" ||
+				ev.key == "ArrowUp" ||
+				ev.key == "ArrowDown") &&
+			container.matches(":hover")
+		) {
+			ev.stopPropagation();
+			ev.preventDefault();
+
+			let field = 0;
+			let step = 0;
+			if (ev.key == "ArrowLeft" || ev.key == "ArrowRight") {
+				field = Number(settings.getFieldValue("default-increment"));
+				if (ev.key == "ArrowLeft") {
+					field = -field;
+				}
+			} else if (ev.key == "ArrowUp" || ev.key == "ArrowDown") {
+				field = Number(settings.getFieldValue("shift-increment"));
+				if (ev.key == "ArrowDown") {
+					field = -field;
+				}
+			}
+			step = field / 100;
+			stepVolume(step);
+		}
 	});
 }
 
